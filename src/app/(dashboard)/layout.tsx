1e7c236738d99141f737d4f722ff0b0977fdc82b
fabiooc1 <fabiocaldas266@gmail.com>
2026-01-10 11:35:27 -0300

    dashboard page

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,23 @@
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+interface DashboardLayoutProps {
+    children: React.ReactNode
+}
+
+export default async function DashboardLayout({ children }: DashboardLayoutProps) {
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    });
+
+    if (!session) {
+        redirect("/authentication");
+    }
+
+    return (
+        <div className="min-h-screen flex flex-col bg-background">
+            {children}  
+        </div>
+    )
+}
---
+++ src/app/(dashboard)/page.tsx
@@ -0,0 +1,5 @@
+export default async function DashboardPage() {
+    return (
+        <h1>Meus projetos</h1>
+    )
+}
