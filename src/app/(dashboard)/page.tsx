70e2fd7baf8a77c66a06e17437e85eb47074e646
fabiooc1 <fabiocaldas266@gmail.com>
2026-01-10 15:16:05 -0300

    dashboard page

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,5 +1,24 @@
+import { getUserProjects } from "@/actions/projects/get-user-projects";
+import { NewProjectButton } from "./_components/new-project-button";
+import { ProjectsOverview } from "./_components/projects-overview";
+
 export default async function DashboardPage() {
-    return (
-        <h1>Meus projetos</h1>
-    )
+  const projectsPaginationData = await getUserProjects();
+
+  return (
+    <div className="space-y-6">
+      <div className="flex justify-between">
+        <div>
+          <h1 className="text-2xl font-bold">Projetos</h1>
+          <p className="text-md">
+            Gerencie seus projetos e acompanhe o progresso
+          </p>
+        </div>
+
+        <NewProjectButton />
+      </div>
+
+      <ProjectsOverview initialProjectPaginationData={projectsPaginationData} />
+    </div>
+  );
 }
