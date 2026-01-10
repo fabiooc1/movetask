946351370b4653e294e2f4f8d0f35df12469d1b1
fabiooc1 <fabiocaldas266@gmail.com>
2026-01-10 15:17:57 -0300

    eslint format

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,24 +1,15 @@
-import { PrismaClient } from "@/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { nextCookies } from "better-auth/next-js";
-
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-})
-
-const prisma = new PrismaClient({ adapter });
+import { prisma } from "./prisma";
 
 export const auth = betterAuth({
-    emailAndPassword: {
-        enabled: true,
-        autoSignIn: true,
-    },
-    plugins: [
-        nextCookies()
-    ],
-    database: prismaAdapter(prisma, {
-        provider: "postgresql",
-    }),
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: true,
+  },
+  plugins: [nextCookies()],
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
 });
