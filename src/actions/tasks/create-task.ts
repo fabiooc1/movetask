cee7228b30a1e0d54fc842cc5369621db617b956
fabiooc1 <fabiocaldas266@gmail.com>
2026-01-11 14:30:54 -0300

    starting create task action

---
+++ src/actions/tasks/create-task.ts
@@ -0,0 +1,13 @@
+"use server";
+
+async function createTaskAction() {
+  // Verify user authentication
+  // Verify if user is member of the project
+  // Verify dto validity
+  // Verify if project exists
+  // Verify if title is unique within the project
+  // Verify if statusId belongs to the project
+  // Verify if priorityId belongs validity
+  // Verify if assignedToId belongs to project members
+  // Create the task in the database
+}
