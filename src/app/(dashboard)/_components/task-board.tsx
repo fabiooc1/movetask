0b129d1105be0e2bee7c102b7ef842edca36cf23
fabiooc1 <fabiocaldas266@gmail.com>
2026-01-11 14:36:17 -0300

    task board component

--- src/app/(dashboard)/_components/task-board.tsx
+++ src/app/(dashboard)/_components/task-board.tsx
@@ -1,26 +1,50 @@
 "use client";
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { TaskStatusModel } from "@/models/task-status-model";
 import { TaskBoardDot } from "./task-board-dot";
 import { NewTaskButton } from "./new-task-button";
+import { Badge } from "@/components/ui/badge";
+import { TaskBoardEmpty } from "./task-board-empty";
+import { TaskCard } from "./task-card";
+import { TaskProjectStatusModel } from "@/models/task-project-status-model";
 
 interface TaskBoardProps {
-  status: TaskStatusModel;
+  projectId: number;
+  status: TaskProjectStatusModel;
 }
 
-export function TaskBoard({ status }: TaskBoardProps) {
+export function TaskBoard({ projectId, status }: TaskBoardProps) {
+  let cardContent = <TaskBoardEmpty />;
+
+  if (status.tasks.length > 0) {
+    cardContent = (
+      <>
+        {status.tasks.map((t) => (
+          <CardContent key={t.id}>
+            <TaskCard task={t} />
+          </CardContent>
+        ))}
+      </>
+    );
+  }
+
   return (
-    <Card className="w-full h-full flex flex-col">
+    <Card className="w-107.5 h-full flex flex-col">
       <CardHeader className="flex flex-row items-center justify-between gap-4 pb-4">
         <CardTitle className="flex items-center gap-2 text-base">
           <TaskBoardDot color={status.color} />
           {status.name}
+          <Badge>{status.tasks.length}</Badge>
         </CardTitle>
 
-        <NewTaskButton buttonLayout="icon" />
+        <NewTaskButton
+          buttonLayout="icon"
+          projectId={projectId}
+          statusId={status.id}
+        />
       </CardHeader>
-      <CardContent className="flex-1 flex flex-col gap-3 overflow-y-auto"></CardContent>
+
+      {cardContent}
     </Card>
   );
 }
