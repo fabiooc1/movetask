7fed31700a077a53e0379709d716abbb3cb557c1
fabiooc1 <fabiocaldas266@gmail.com>
2026-01-10 21:33:09 -0300

    setter app providers

---
+++ src/components/providers.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { queryClient } from "@/lib/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
+
+interface ProvidersProps {
+  children: React.ReactNode;
+}
+
+export function Providers({ children }: ProvidersProps) {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
