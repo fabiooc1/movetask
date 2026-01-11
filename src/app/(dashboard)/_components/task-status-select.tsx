34d3f97fb77be1717d9db985ad9b50bc2543b902
fabiooc1 <fabiocaldas266@gmail.com>
2026-01-11 14:38:34 -0300

    task status select

---
+++ src/app/(dashboard)/_components/task-status-select.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import {
+  SelectItem,
+  Select,
+  SelectContent,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useGetTaskStatus } from "@/hooks/use-get-task-status";
+
+interface TaskStatusSelectProps {
+  value?: string;
+  onChange?: (value: string) => void;
+}
+
+export function TaskStatusSelect({}: TaskStatusSelectProps) {
+  const { isLoading, data: status } = useGetTaskStatus();
+
+  if (isLoading) {
+    return <Skeleton className="w-full h-9" />;
+  }
+
+  if (!status) {
+    return null;
+  }
+
+  console.log(status);
+
+  return (
+    <Select>
+      <SelectTrigger className="w-full">
+        <SelectValue placeholder="Selecione o status da tarefa" />
+      </SelectTrigger>
+
+      <SelectContent>
+        {status.map((ts) => (
+          <SelectItem key={ts.id} value={ts.id.toString()}>
+            {ts.name}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+}
